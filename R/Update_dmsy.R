6f2c93937391150caa947d5ee38eabd68442d2e5
Haikun Xu <haikunxu1990@gmail.com>
2021-09-08 11:37:01 -0700

    Update_dmsy tested

--- R/Update_dmsy.R
+++ R/Update_dmsy.R
@@ -1,24 +1,25 @@
+# tested on Sep. 8 2021
+
 library(r4ss)
 
 Dir <- "D:/OneDrive - IATTC/IATTC/2021/UpdateAssessment/SS/"
 NewDir <- "D:/OneDrive - IATTC/IATTC/2021/UpdateAssessment/SS(dmsy)/"
 SSDir <- "D:/OneDrive - IATTC/Git/UpdateAssessment/Document/dmsy/"
-model <- c("R","RG","RM","RS","G","M1","M2","S","L","LG","LM","LS")
-model_name <- c("R","R-GC","R-MA","R-DS","GC","MJ","MA","DS","L","L-GC","L-MA","L-DS")
-model_name2 <- c("Env-Fix","Env-Gro","Env-Mrt","Env-Sel","Gro","Mov","Mrt","Sel","Srt-Fix","Srt-Gro","Srt-Mrt","Srt-Sel")
+model <- c("Env-Fix","Env-Gro","Env-Mrt","Env-Sel","Gro","Mov","Mrt","Sel","Srt-Fix","Srt-Gro","Srt-Mrt","Srt-Sel")
 steepness <- seq(1,0.7,-0.1)
 
 converge <- matrix(1,nrow=length(model),ncol=length(steepness))
 converge[1,2:4] <- 0
 converge[9,4] <- 0
 
+dir.create(NewDir)
+
 for (m in 1:1) {
   for (s in 1:1) {
-    Path <- paste0(Dir,model[m],"-",toString(steepness[s]))
-    NewPath <- paste0(NewDir,model_name[m],"-",toString(steepness[s]))
-    print(NewPath)
-    
     if (converge[m, s]) {
+      Path <- paste0(Dir,model[m],"-",toString(steepness[s]))
+      NewPath <- paste0(NewDir,model[m],"-",toString(steepness[s]))
+      print(NewPath)
 
       # step 1: creata a new folder for each run
       unlink(NewPath, recursive = TRUE, force = TRUE)
@@ -27,13 +28,13 @@ for (m in 1:1) {
                 paste0(SSDir, "/starter.ss"),
                 paste0(Path, "/BET-EPO.ctl"),
                 paste0(Path, "/BET-EPO.dat"),
-                paste0(SSDir, "/ss.exe"))
+                paste0(Path, "/ss.exe"))
       file.copy(from = files, to = NewPath)
       
       # step 2: change par file
-      print("Change the starter file to 1 (from par)!!!")
+      print("Change the starter file to 1 (from par) and turn off estimation!!!")
       
-      ParDir <- paste0(Path, "ss.par")
+      ParDir <- paste0(Path, "/ss.par")
       ParFile <- readLines(ParDir, warn = F)
       
       Rep <- r4ss::SS_output(dir = Path, ncols = 400, covar = T, printstats = F, verbose = FALSE)
@@ -58,5 +59,6 @@ for (m in 1:1) {
       setwd(NewPath)
       command <- paste("cd", NewPath, "& go_noHess.bat", sep = " ")
       ss <- shell(cmd = command, intern = T, wait = T)
+    }
   }
 }
